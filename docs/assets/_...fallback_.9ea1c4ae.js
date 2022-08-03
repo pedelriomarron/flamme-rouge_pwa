cdf50a1d31138fc5a070488e9fa5a179204b451c
Pedro del Rio <pedelriomarron@gmail.com>
2022-08-03 03:44:52 +0200

    dev5

--- docs/assets/_...fallback_.9ea1c4ae.js
+++
@@ -1,1 +0,0 @@
-import{S as a,i as r,s as i,k as l}from"./index.4e9f76e2.js";import{g as u}from"./index.483336f9.js";function c(t,o,e){let s;return l(t,u,n=>e(0,s=n)),s("/"),[]}class m extends a{constructor(o){super(),r(this,o,c,null,i,{})}}export{m as default};
--- vite.config.ts
+++ vite.config.ts
@@ -7,9 +7,11 @@ import { readFileSync } from 'fs'
 
 const pwaManifest = {
   name: 'Flamme Rouge PWA',
-  short_name: 'flamme',
+  short_name: 'Flamme Rouge PWA',
   description: 'Aplicación en ionic con pwa para el juego de mesa flamme rouge.',
   theme_color: '#000000',
+  start_url: "/flamme-rouge_pwa/index.html",
+  scope: "/",
   icons: [
     {
       src: './assets/img/great-success.png',
